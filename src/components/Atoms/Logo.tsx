598ed3759ca9fab09e417f223e255cf8dc6f8eb1
Gabriel <gabrigomez15@gmail.com>
2023-08-31 10:50:28 -0300

    feat: start NavBar component

--- src/components/Atoms/Logo.tsx
+++ src/components/Atoms/Logo.tsx
@@ -3,7 +3,7 @@ import logo from '../../assets/logo.png';
 export const Logo = () => {
   return (
     <a href=''>
-      <img src={logo} alt="" />
+      <img src={logo} alt="" title='Meu emprego começa aqui! :D' />
     </a>
   )
 }
--- src/components/Molecules/NavBar.tsx
+++ src/components/Molecules/NavBar.tsx
@@ -1,9 +1,11 @@
-import { Logo } from "../Atoms/Logo"
+import { Logo } from "../Atoms/Logo";
+import { FiMenu } from "react-icons/fi"
 
 export const NavBar = () => {
   return (
-    <nav>
+    <nav className="flex items-center justify-between h-[60px] mx-3">
       <Logo />
+      <FiMenu className="text-2xl text-slate-800 hover:bg-slate-200 duration-500 rounded-full p-2 h-10 w-10 cursor-pointer" />
     </nav>
   )
 }
