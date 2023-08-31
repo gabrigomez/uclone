680d899577030a28b8fca3673d36545bbf75a4bd
Gabriel <gabrigomez15@gmail.com>
2023-08-31 19:33:54 -0300

    fix: placheholder text color

--- src/components/Molecules/SearchEvent.tsx
+++ src/components/Molecules/SearchEvent.tsx
@@ -5,14 +5,14 @@ export const SearchEvent = () => {
   return (
     <div className="searchEvent flex items-center justify-between rounded-full w-11/12">
       <input 
-        className='rounded-full pl-5 w-36'
+        className='rounded-full pl-5 w-36 outline-none placeholder:text-gray-700'
         type="text"
         placeholder="Buscar Evento" 
         name="" 
         id="" 
       />
       <button
-        className='flex items-center bg-orange-500 hover:bg-orange-400 duration-300 rounded-full w-40 m-1 p-2'
+        className='flex items-center bg-orange-400 hover:bg-orange-300 duration-300 rounded-full w-40 m-1 p-2'
       > 
         <div className="flex items-center">
           <CiLocationOn className="text-white mr-2" />
