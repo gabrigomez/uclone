78da79884a356d2516bd5ae4a2ec37df5f5223f7
Gabriel <gabrigomez15@gmail.com>
2023-09-01 15:09:32 -0300

    feat: mock fake data to events

--- src/components/Organisms/EventItem.tsx
+++ src/components/Organisms/EventItem.tsx
@@ -1,25 +1,35 @@
-export const EventItem = () => {
+export interface EventItemProps {
+  price?: string,
+  img: string,
+  avatar: string,
+  name: string,
+  date: string,
+  locale: string,
+}
+
+export const EventItem = ({...props}: EventItemProps) => {
   return (
     <a className="flex flex-col relative">
-      <div className="absolute right-1 top-1 bg-slate-50 rounded-lg font-semibold text-sm p-1">
-        R$30,00
-      </div>
-      <div className="flex items-center font-bold justify-center text-white absolute right-5 bottom-20 border-4 border-white bg-black rounded-full w-14 h-14">
-        G
+      <div className={`${props.price ? 'absolute right-1 top-1 bg-slate-50 rounded-lg font-semibold text-sm p-1' : ''}`}>
+        {props.price}
       </div>
       <img 
+        className="flex items-center font-bold justify-center text-white absolute right-5 bottom-20 border-4 border-white bg-black rounded-full w-14 h-14"
+        src={props.avatar}
+      />
+      <img 
         className="bg-red-500 h-[160px] rounded-md" 
-        src="https://img.freepik.com/free-vector/flat-hand-drawn-psychedelic-groovy-background_23-2148850121.jpg?w=2000"
+        src={props.img}
       />      
       <div className="flex flex-col h-2/5 px-3 py-2">
         <p className="font-bold text-lg">
-          Face Melting Destroyer
+          {props.name}
         </p>
         <p className="text-md text-sky-500">
-          Sábado, 11 de novembro
+          {props.date}
         </p>
         <p className="text-md text-gray-400">
-          The 169 house
+          {props.locale}
         </p>
       </div>
     </a>
