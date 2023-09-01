d5bb79c6894df7c207057effe3ef21ac47936616
Gabriel <gabrigomez15@gmail.com>
2023-09-01 10:00:24 -0300

    styles: fix navBar and SearchEvent responsiveness

--- src/components/Molecules/NavBar.tsx
+++ src/components/Molecules/NavBar.tsx
@@ -3,7 +3,7 @@ import { FiMenu } from "react-icons/fi";
 
 export const NavBar = () => {
   return (
-    <nav className="flex items-center justify-between h-[60px] mx-3">
+    <nav className="flex items-center justify-between h-[60px] mx-3 xl:navBarMargin">
       <Logo />
       <FiMenu className="text-2xl text-slate-800 hover:bg-slate-200 duration-500 rounded-full p-2 h-10 w-10 cursor-pointer" />
     </nav>
