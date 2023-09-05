2fe5a1c6bcb090d36054222972b167f23e993c25
Gabriel <gabrigomez15@gmail.com>
2023-09-05 15:01:25 -0300

    fix: implement filter events logic in right place

--- src/components/Organisms/Events.tsx
+++ src/components/Organisms/Events.tsx
@@ -1,31 +1,17 @@
-import { useState } from "react";
 import { EventItem, EventItemProps } from "./EventItem";
 import { Button } from "../Atoms/Button";
 
 interface EventsProps {
-  events: Array<EventItemProps> | null
+  events: Array<EventItemProps> | null,
+  isOverEvents: boolean,
+  loadMore: () => void,
 }
 
-export const Events = ({events}: EventsProps) => {
-  const [eventList, setEventList] = useState(events?.slice(0,5));
-  const [loadCount, setLoadCount] = useState(5);
-  
-  const [isOverEvents, setIsOverEvents] = useState(false);
-
-  const loadMore = () => {
-    setLoadCount(loadCount + 5);
-    
-    if(eventList!.length < events!.length) {
-      setEventList(events?.slice(0, loadCount + 5));
-    } else {
-      setIsOverEvents(true);
-    }
-  }
-
+export const Events = ({...props}: EventsProps) => {
   return (
     <div className="bg-white mt-20 mx-3 xl:eventMargin relative">
       <div className="grid gap-6 grid-cols-1 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {eventList?.map((event) => {
+        {props.events?.map((event) => {
           return (
             <EventItem
               key={event.name}
@@ -39,9 +25,9 @@ export const Events = ({events}: EventsProps) => {
           )
         })}
       </div>
-      {!isOverEvents ? (
+      {!props.isOverEvents ? (
         <Button
-          onClick={loadMore}
+          onClick={props.loadMore}
           text="Carregar mais eventos"
           className="w-full mt-4 p-2 rounded-full border border-sky-500 hover:bg-sky-300 text-sky-600 duration-200"
         />
--- src/components/Organisms/Main.tsx
+++ src/components/Organisms/Main.tsx
@@ -3,14 +3,27 @@ import { Banner } from "../Molecules/Banner";
 import { SearchEvent } from "../Molecules/SearchEvent";
 import { Events } from "./Events";
 
-import { eventItems } from "../../data";
+import { eventItems as events } from "../../data";
 import { EventItemProps } from "./EventItem";
 import { NoResult } from "../Atoms/NoResult";
 
 export const Main = () => {
   const [search, setSearch] = useState("");
+  const [eventList, setEventList] = useState(events?.slice(0,5));
+  const [loadCount, setLoadCount] = useState(5);  
+  const [isOverEvents, setIsOverEvents] = useState(false);
 
-  const filteredResult = eventItems.filter((event: EventItemProps) =>
+  const loadMore = () => {
+    setLoadCount(loadCount + 5);
+    
+    if(eventList!.length < events!.length) {
+      setEventList(events?.slice(0, loadCount + 5));
+    } else {
+      setIsOverEvents(true);
+    }
+  }
+
+  const filteredResult = eventList.filter((event: EventItemProps) =>
     event.name.toLowerCase().includes(search.toLowerCase())
   );
 
@@ -23,7 +36,11 @@ export const Main = () => {
       <Banner />
       <SearchEvent func={pull_data} />
       {filteredResult.length > 0 ? (
-        <Events events={filteredResult} />
+        <Events 
+          events={filteredResult} 
+          isOverEvents={isOverEvents} 
+          loadMore={loadMore} 
+        />
       ) : (
         <NoResult />
       )}
