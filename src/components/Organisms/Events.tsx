4fc9564f743f43b519477bc26dfe8f8299e40817
Gabriel <gabrigomez15@gmail.com>
2023-09-05 14:41:14 -0300

    feat: add load more functionality

--- src/components/Organisms/Events.tsx
+++ src/components/Organisms/Events.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { EventItem, EventItemProps } from "./EventItem";
 
 interface EventsProps {
@@ -5,10 +6,25 @@ interface EventsProps {
 }
 
 export const Events = ({events}: EventsProps) => {
+  const [eventList, setEventList] = useState(events?.slice(0,5));
+  const [loadCount, setLoadCount] = useState(5);
+  
+  const [message, setMessage] = useState("Carregar mais eventos");
+
+  const loadMore = () => {
+    setLoadCount(loadCount + 5);
+    
+    if(eventList!.length < events!.length) {
+      setEventList(events?.slice(0, loadCount + 5));
+    } else {
+      setMessage("Todos os eventos carregados");
+    }
+  }
+
   return (
     <div className="bg-white mt-20 mx-3 xl:eventMargin relative">
       <div className="grid gap-6 grid-cols-1 xs:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        {events?.map((event) => {
+        {eventList?.map((event) => {
           return (
             <EventItem
               key={event.name}
@@ -22,8 +38,8 @@ export const Events = ({events}: EventsProps) => {
           )
         })}
       </div>
-      <button className="w-full mt-4 p-2 rounded-full border border-sky-500 hover:bg-sky-300 text-sky-600 duration-200">
-        Carregar mais eventos
+      <button onClick={loadMore} className="w-full mt-4 p-2 rounded-full border border-sky-500 hover:bg-sky-300 text-sky-600 duration-200">
+        {message}
       </button>
     </div>
   )
